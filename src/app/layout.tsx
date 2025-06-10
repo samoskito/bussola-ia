8a2fe66a6a253f2fcfa4884af6061b2d764f3b7b
Samuel Choairy <samuelchoairy@MacBook-Pro-de-Samuel.local>
2025-06-09 21:17:34 -0300

    bussola-ia30

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './tailwind.css'
 import { AuthProvider } from '@/contexts/AuthContext'
 import { Toaster } from 'react-hot-toast'
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -45,6 +46,7 @@ export default function RootLayout({
             }}
           />
           {children}
+          <Analytics />
         </AuthProvider>
       </body>
     </html>
